c19fa8236b0bb7512a2b08817d706d1c81cf77f7
Johanna Merluza <johanna.merluza@gmail.com>
2023-04-09 00:51:25 -0400

    add meal, layout, and UI components

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from './MealsSummary.module.css';
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Food Delivered</h2>
+      <p>Choose a meal and enjoy from home</p>
+      <p>All meals taste good</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
